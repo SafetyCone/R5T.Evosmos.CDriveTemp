7539a3d6eab94bee9fd0f54fc9a42b30f968018a
MinexAutomation <minex.automation@gmail.com>
2020-01-23 00:37:22 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Evosmos.CDriveTemp.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Evosmos.CDriveTemp.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Evosmos.CDriveTemp/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Evosmos.CDriveTemp
+{
+    public class Class1
+    {
+    }
+}
