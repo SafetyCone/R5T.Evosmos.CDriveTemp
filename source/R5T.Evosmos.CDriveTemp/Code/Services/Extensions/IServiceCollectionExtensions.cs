ea2bb381ddca9f927e3f32d778d5b877fe3984f5
MinexAutomation <minex.automation@gmail.com>
2020-01-30 16:43:54 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Evosmos.CDriveTemp/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Evosmos.CDriveTemp/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Dacia;
 using R5T.Evosmos.Thessaloniki;
 using R5T.Lombardy.Standard;
 using R5T.Thessaloniki.CDrive;
@@ -16,13 +17,21 @@ namespace R5T.Evosmos.CDriveTemp
         /// </summary>
         public static IServiceCollection AddTemporaryDirectoryFilePathProvider(this IServiceCollection services)
         {
-            services
-                .AddSingleton<ITemporaryDirectoryFilePathProvider, TemporaryDirectoryFilePathProvider>()
-                .AddTemporaryDirectoryPathProvider()
-                .AddStringlyTypedPathOperator()
+            services.AddThessalonikiTemporaryDirectoryFilePathProvider(
+                services.AddTemporaryDirectoryPathProviderAction(),
+                services.AddStringlyTypedPathOperatorAction())
                 ;
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds an <see cref="ITemporaryDirectoryFilePathProvider"/> service.
+        /// </summary>
+        public static ServiceAction<ITemporaryDirectoryFilePathProvider> AddTemporaryDirectoryFilePathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ITemporaryDirectoryFilePathProvider>(() => services.AddTemporaryDirectoryFilePathProvider());
+            return serviceAction;
         }
     }
 }
